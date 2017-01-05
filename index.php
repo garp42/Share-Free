b43378aefb8777e746b28b4378b0ed2683ff1160
Haroun <haroun.tmimi@hotmail.com>
2017-01-05 20:52:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="js/script.js" type="text/javascript"></script>
+</head>
+<body>
+	<header>
+		<a href="#"><h1 id="title">Share Free</h1></a>
+		<input type="search" placeholder="Entrez votre recherche ici" id="search-bar">
+		<a href="#"><img src="icons/search24.png" id="search-icon"></a>
+		<a href="#"><img src="icons/message32-notification.png" id="message-icon"><label id="msg-ntf">1</label></a>
+		<a href="#"><img src="icons/account32-notification.png" id="account-icon"><label id="account-ntf">1</label></a>
+	</header>
+	<div id="main">
+        <div id="main-img">
+            <a href="#"><img id="" src="img/programmation.png" height="500" width="840"></a>
+            <div id="main-img-description">
+                <h2>Apprendre à programmer</h2>
+                <p>Plongez dans ce grand monde qu'est la progammation ! Avec ce cours, vous deviendrez des programmeurs hors paire. Et ce, en seulement quelques semaines !</p>
+            </div>
+        </div>
+        <div id="main-list">
+            <ul>
+                <a href="#"><li>Apprendre à programmer <label>Matheo1030</label></li></a>
+                <a href="#"><li>Hacker un smartphone Android <label>Steve78</label></li></a>
+                <a href="#"><li>Cracker Photoshop CS6 <label>JohnRpa</label></li></a>
+            </ul>    
+        </div>
+	</div>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(function() {
+            // $('#grand').click( function() {
+            //   $('img').animate({ 'border-width': '100'}, {queue: false, duration: 1500 })
+            //           .animate({ 'width': '+=100'}, 1500);
+            // });
+
+            //icone de compte : changement couleur au passage souris
+           	$('#account-icon').mouseover(function(){
+           		$('#account-icon').attr('src', 'icons/account32-hover.png');
+           	});
+           	$('#account-icon').mouseout(function(){
+           		$('#account-icon').attr('src', 'icons/account32.png');
+           	});
+           	//icone message : changement couleur au passage souris
+           	$('#message-icon').mouseover(function(){
+           		$('#message-icon').attr('src', 'icons/message32-hover.png');
+           	});
+           	$('#message-icon').mouseout(function(){
+           		$('#message-icon').attr('src', 'icons/message32.png');
+           	});
+            
+           	//icone de recherche : changement couleur au passage souris
+            $('#search-icon').mouseover(function(){
+            	$('#search-icon').attr('src', 'icons/search24-hover.png');
+            });
+            $('#search-icon').mouseout(function(){
+            	$('#search-icon').attr('src', 'icons/search24.png');
+            });
+            //quand on clique sur l'icone, on est focus sur la recherche
+            $('#search-icon').click(function(){
+            	$('#search-bar').focus();
+            });
+
+            //barre de recherche : changement couleur au focus
+            $('#search-bar').focus(function(){
+            	$('#search-bar').css('background-color', 'white');
+            	$('#search-bar').css('color', 'black');
+            });
+            $('#search-bar').blur(function(){
+            	$('#search-bar').css('background-color', '#898282');
+            	$('#search-bar').css('color', 'white');
+            });
+    
+            $('#main-img').mouseover(function(){
+                $('#main-img-description h2').css('opacity', '1');
+                $('#main-img-description p').css('opacity', '1');
+                $('#main-img-description').animate({ 'height': '200' }, {queue:false, duration: 1000});
+                $('#main-img img').css('filter', 'blur(5px)').css('transition', '1s');
+            });
+            $('#main-img').mouseout(function(){
+                $('#main-img-description').animate({ 'height': '0'}, {queue:false, duration: 1000}).next('#main-img-description h2').css('opacity', '0');
+                $('#main-img-description p').css('opacity', '0');
+                $('#main-img img').css('filter', 'blur(0px)').css('transition', '1s');
+            });
+        });
+          
